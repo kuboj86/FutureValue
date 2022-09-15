9e0224e80dec754c53607739879c718a6d06944c
Jason Kubo <kuboj86@gmail.com>
2022-09-15 00:03:23 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureValue = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FutureValue = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FutureValue = 0;
+            }
+            return View(model);
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
